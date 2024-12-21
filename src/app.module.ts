0aff73e8b79129a42ab2b19faf09e1d28c3ea6c5
Dami <damilarelamine@gmail.com>
2024-12-21 16:03:16 +0100

    chore: setup node-config-ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,11 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ProductModule } from './modules/product/product.module';
 import { CategoryModule } from './modules/category/category.module';
 import { UserModule } from './modules/user/user.module';
+import { config } from 'node-config-ts';
 
 @Module({
   imports: [
-    MongooseModule.forRoot(process.env.MONGO_URI),
+    MongooseModule.forRoot(config.MONGO_URI),
     ProductModule,
     CategoryModule,
     UserModule,
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { config } from 'node-config-ts';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  await app.listen(parseInt(process.env.PORT));
+  await app.listen(config.PORT);
 }
 bootstrap();
