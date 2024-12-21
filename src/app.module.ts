151c28d9e69aec8bc3ef09a7b15f8aaaa38db594
Dami <damilarelamine@gmail.com>
2024-12-21 14:55:21 +0100

    chore: folder structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { MongooseModule } from '@nestjs/mongoose';
-import { ProductModule } from './product/product.module';
-import { CategoryModule } from './category/category.module';
-import { UserModule } from './user/user.module';
+import { ProductModule } from './modules/product/product.module';
+import { CategoryModule } from './modules/category/category.module';
+import { UserModule } from './modules/user/user.module';
 
 @Module({
   imports: [
