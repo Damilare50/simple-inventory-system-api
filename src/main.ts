f642ac4347a66c3407196632f5708add33e7f751
Dami <damilarelamine@gmail.com>
2024-12-21 23:54:51 +0100

    chore: installclass validator

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { config } from 'node-config-ts';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
   await app.listen(config.PORT);
 }
