1c0402aa496d02344820976b82a5098e755b7562
Dami <damilarelamine@gmail.com>
2024-12-21 14:43:24 +0100

    chore: scaffold product module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ProductModule } from './product/product.module';
 
 @Module({
-  imports: [MongooseModule.forRoot(process.env.MONGO_URI)],
+  imports: [MongooseModule.forRoot(process.env.MONGO_URI), ProductModule],
   controllers: [AppController],
 })
 export class AppModule {}
